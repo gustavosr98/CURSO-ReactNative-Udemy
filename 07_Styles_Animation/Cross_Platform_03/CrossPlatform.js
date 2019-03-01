e78abdcce111a89d3dc03400ec3a2f7b10f7bd50
Gustavo Sánchez <gsanchez@conectium.com>
2019-03-01 11:39:51 -0400

    Cross Platform

---
+++ 07_Styles_Animation/Cross_Platform_03/CrossPlatform.js
@@ -0,0 +1,15 @@
+import { Platform } from 'react-native'
+
+if (PLatform.OS === 'android') {
+    // APROVECHAR EL PLatform.OS PARA CAMBIAR DIFERENTES ICONOS, U OPCIONES ESPECIFICAS A UN S.O.
+    // PLatform.OS PUEDE SER IGUAL A android, ios, macos, windows, web.
+}
+
+// ********************
+// TAMBIEN APROVECHAR LOS DIFERENTES ENTRY POINTS
+// index.android.js y index.ios.js 
+
+// *******************
+// EXISTEN "LIBRERIAS CROSS PLATFORM UI"
+// POR EJEMPLO NativeBase.io
+// HAY MAS, PERO TODAS PUEDEN TRAER VARIOS BUGS YA QUE REACT NATIVE CAMBIA MUCHO
